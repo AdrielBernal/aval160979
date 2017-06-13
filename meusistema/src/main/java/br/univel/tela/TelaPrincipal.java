ff363ad3567d4827e2b7d01aab27cec62cdc2f1f
AdrielBernal <dry_b@hotmail.com>
2017-06-12 21:57:48 -0300

    Criação das telas.

---
+++ meusistema/src/main/java/br/univel/tela/TelaPrincipal.java
@@ -0,0 +1,81 @@
+package br.univel.tela;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTabbedPane;
+import javax.swing.border.EmptyBorder;
+
+public class TelaPrincipal extends JFrame {
+
+	private JPanel contentPane;
+	private JTabbedPane tabbedPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					TelaPrincipal frame = new TelaPrincipal();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public TelaPrincipal() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(new BorderLayout(0, 0));
+
+		JPanel panel = new JPanel();
+		contentPane.add(panel, BorderLayout.NORTH);
+		panel.setLayout(new FlowLayout(FlowLayout.LEADING, 5, 5));
+
+		JButton btnContato = new JButton("Contato");
+		btnContato.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				adicionarAbaAluno();
+			}
+		});
+		panel.add(btnContato);
+
+		tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+		contentPane.add(tabbedPane, BorderLayout.CENTER);
+	}
+
+	protected void adicionarAbaAluno() {
+		JPanel cadastroAluno = new CadastroAluno();
+
+		PainelWrapper wrapper = new PainelWrapper();
+		wrapper.setConteudo(cadastroAluno);
+		wrapper.setTitulo("Cadastro de Alunos");
+
+		wrapper.setAcaoFechar(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				tabbedPane.remove(wrapper);
+			}
+		});
+
+		tabbedPane.addTab("Contato", wrapper);
+
+	}
+
+}
